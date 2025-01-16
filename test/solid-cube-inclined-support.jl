443cf47218de1ac56d1146f96b5ad30ce99bcb43
Brian Phung <bphung@sandia.gov>
2025-01-16 09:53:37 -0700

    explicit dynamics inclined support DBC test

--- test/solid-cube-inclined-support.jl
+++ test/solid-cube-inclined-support.jl
@@ -83,7 +83,7 @@ end
     
     # These displacements are obtained from an identical problem with
     # BCs coincident with the lab basis
-    reference_displacements = reference_displacements = [
+    reference_displacements = [
         0.19999999999999996, 0.0, 0.0, 0.19999999999999996, 0.0, 0.0,
         0.19999999999999996, 0.0, 0.0, 0.19999999999999996, 0.0, 0.0,
         -0.002490463082911527, -0.05480993185221903, 0.05480993185221887,
@@ -127,6 +127,67 @@ end
     displacements = model.global_transform' * integrator.displacement
 
     # Assert the displacement array matches the reference displacements
-    @test displacements ≈ correct_displacements atol=1e-5
+    @test displacements ≈ correct_displacements atol=1e-6
+    
+end
+
+@testset "explicit-dynamics-inclined-support" begin
+    cp("../examples/single/explicit-dynamic-solid/cube_compression_inclined_support_30_degrees/cube.yaml", "cube.yaml", force=true)
+    cp("../examples/single/explicit-dynamic-solid/cube_compression_inclined_support_30_degrees/cube-1.g", "cube-1.g", force=true)
+    simulation = Norma.run("cube.yaml")
+    integrator = simulation.integrator
+    model = simulation.model
+    rm("cube.yaml")
+    rm("cube-1.g")
+    rm("cube.e")
+    
+    # These displacements are obtained from an identical problem with
+    # BCs coincident with the lab basis
+    reference_displacements = reference_displacements = [
+        0.09999999999999998, 0.0, 0.0, 0.09999999999999998, 0.0, 0.0,
+        0.09999999999999998, 0.0, 0.0, 0.09999999999999998, 0.0, 0.0,
+        -0.00014160957605313936, -0.029544643718552567, 0.029544643718552227,
+        0.002463135608373333, -0.029543111106731912, -2.3591795184074754e-18,
+        0.005069074149454265, 8.364919867886994e-19, 1.0077938483732418e-18,
+        0.0024631356083732207, 1.1608658478934337e-18, 0.02954311110673235,
+        0.09999999999999998, 0.0, 0.0, 0.09999999999999998, 0.0, 0.0,
+        -0.00014160957605335978, -0.029544643718552567, -0.029544643718552227,
+        0.0024631356083731084, 1.901706569995555e-18, -0.02954311110673235,
+        0.09999999999999998, 0.0, 0.0, 0.09999999999999998, 0.0, 0.0,
+        0.0024631356083731648, 0.029543111106732488, -1.8534063173092413e-19,
+        -0.00014160957605336002, 0.029544643718552283, 0.029544643718552005,
+        0.09999999999999998, 0.0, 0.0, -0.00014160957605336043, 0.029544643718552227,
+        -0.029544643718552005, -0.1, -0.028135723132685943, 0.02813572313268593,
+        -0.1, -0.02820846645732876, -2.6227164884318194e-17, -0.1,
+        3.0273068774011585e-18, -6.509032549062432e-18, -0.1, 3.886557742305287e-18,
+        0.02820846645732832, -0.1, -0.02813572313268617, -0.02813572313268593,
+        -0.1, 3.762101741244808e-18, -0.02820846645732832, -0.1,
+        0.028208466457328753, -2.773270502132163e-18, -0.1, 0.028135723132685832,
+        0.028135723132685704, -0.1, 0.028135723132685805, -0.028135723132685704
+    ]
+    
+    
+    # Rotate these displacements
+    angle = 30. * π / 180
+    c = cos(angle)
+    s = sin(angle)
+    # Rotate about z
+    local_rotation_matrix = [ c s 0; -s c 0 ; 0 0 1]
+    global_rotation = zeros((81,81))
+    for i in range(1,27)
+        base = (i-1)*(3) + 1
+        global_rotation[base:(base+2), base:(base+2)] = local_rotation_matrix
+    end
+
+    correct_displacements = global_rotation' * reference_displacements
+
+    local_stiffness_displacements = integrator.displacement
+    # Displacements in the integrator are stored in the local frame
+    # (i.e., inclined support frame). Use the model global_transform
+    # to return them to the global frame
+    displacements = model.global_transform' * integrator.displacement
+
+    # Assert the displacement array matches the reference displacements
+    @test displacements ≈ correct_displacements atol=1e-6
     
 end
