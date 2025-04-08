8cdd8ba52a0cb81ac9217bda99b6ea57c2a61260
Eric Joshua Parish <ejparis@sandia.gov>
2025-04-08 10:09:46 -0500

    refactor of nn opinf from previous branch

--- src/ics_bcs_types.jl
+++ src/ics_bcs_types.jl
@@ -26,6 +26,19 @@ mutable struct SMDirichletBC <: RegularBoundaryCondition
     acce_num::Num
 end
 
+mutable struct SMOpInfDirichletBC <: RegularBoundaryCondition 
+    node_set_name::String
+    offset::Int64
+    node_set_id::Int64
+    node_set_node_indices::Vector{Int64}
+    disp_num::Num
+    velo_num::Num
+    acce_num::Num
+    fom_bc::SMDirichletBC
+    nn_model::Any
+    basis::Any
+end
+
 mutable struct SMDirichletInclined <: RegularBoundaryCondition
     node_set_name::String
     node_set_id::Int64
--- src/model_types.jl
+++ src/model_types.jl
@@ -108,3 +108,20 @@ mutable struct LinearOpInfRom <: OpInfModel
     reference::Matrix{Float64}
     inclined_support::Bool
 end
+
+mutable struct NeuralNetworkOpInfRom <: OpInfModel
+    nn_model::Any
+    basis::Array{Float64}
+    reduced_state::Vector{Float64}
+    reduced_velocity::Vector{Float64}
+    reduced_boundary_forcing::Vector{Float64}
+    #internal_force not used, but include to ease interfacing in Schwarz
+    internal_force::Vector{Float64}
+    free_dofs::BitVector
+    boundary_conditions::Vector{BoundaryCondition}
+    time::Float64
+    failed::Bool
+    fom_model::SolidMechanics
+    reference::Matrix{Float64}
+    inclined_support::Bool
+end
