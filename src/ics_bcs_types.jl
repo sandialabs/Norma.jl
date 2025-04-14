f44662f8701b50a94a32194731a9cadc975595aa
Eric Joshua Parish <ejparis@sandia.gov>
2025-04-14 11:49:58 -0500

    added NN for Schwarz

--- src/ics_bcs_types.jl
+++ src/ics_bcs_types.jl
@@ -87,6 +87,21 @@ mutable struct SMOverlapSchwarzBC <: OverlapSchwarzBoundaryCondition
     swap_bcs::Bool
 end
 
+mutable struct SMOpInfOverlapSchwarzBC <: OverlapSchwarzBoundaryCondition
+    side_set_name::String
+    side_set_node_indices::Vector{Int64}
+    coupled_nodes_indices::Vector{Vector{Int64}}
+    interpolation_function_values::Vector{Vector{Float64}}
+    coupled_subsim::Simulation
+    subsim::Simulation
+    is_dirichlet::Bool
+    swap_bcs::Bool
+    fom_bc::SMOverlapSchwarzBC
+    nn_model::Any
+    basis::Any
+end
+
+
 mutable struct SMNonOverlapSchwarzBC <: NonOverlapSchwarzBoundaryCondition
     side_set_id::Int64
     side_set_node_indices::Vector{Int64}
