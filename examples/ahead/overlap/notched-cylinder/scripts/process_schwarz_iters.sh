8fb4a115deadf6f7e6788c5e671655995e32074c
Irina Tezaur <ikalash@sandia.gov>
2025-04-16 10:53:49 -0700

    Modifying post-proc scripts so that they correctly extract # Schwarz iterations in numerical format by getting rid of the emoticon output that is produced now by Norma.

--- examples/ahead/overlap/notched-cylinder/scripts/process_schwarz_iters.sh
+++ examples/ahead/overlap/notched-cylinder/scripts/process_schwarz_iters.sh
@@ -3,4 +3,7 @@
 
 grep "Performed" out >& schwarz-iters.txt 
 sed -i "s/Performed//g" schwarz-iters.txt
-sed -i "s/Schwarz iterations//g" schwarz-iters.txt
+sed -i "s/Schwarz Iterations//g" schwarz-iters.txt
+awk '{print substr($0, 2)}' schwarz-iters.txt >& a 
+mv a schwarz-iters.txt 
+
