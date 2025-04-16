42c7f151fd2ef81fbfdff4eafcf537b333901246
Irina Tezaur <ikalash@sandia.gov>
2025-04-16 10:59:35 -0700

    Minor correction to scripts.

--- examples/ahead/overlap/laser-weld/scripts/process_schwarz_iters.sh
+++ examples/ahead/overlap/laser-weld/scripts/process_schwarz_iters.sh
@@ -4,6 +4,6 @@
 grep "Performed" out >& schwarz-iters.txt 
 sed -i "s/Performed//g" schwarz-iters.txt
 sed -i "s/Schwarz Iterations//g" schwarz-iters.txt
-awk '{print substr($0, 2)}' schwarz-iters.txt >& a 
+awk '{print substr($0, 6)}' schwarz-iters.txt >& a 
 mv a schwarz-iters.txt 
 
