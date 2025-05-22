b60e3394fea98c4bce9d0c9d6523035fe521b726
Irina Tezaur <ikalash@sandia.gov>
2025-05-22 16:47:49 -0700

    Correcting and expanding bracket non-overlapping tests.

--- test/schwarz-ahead-overlap-dynamic-bracket.jl
+++ test/schwarz-ahead-overlap-dynamic-bracket.jl
@@ -6,7 +6,7 @@
 
 using YAML
 
-@testset "Schwarz AHeaD Overlap Dynamic Bracket TET4-TET4 Dissipative Newmark" begin
+@testset "Schwarz AHeaD Overlap Dynamic Bracket HEX8-TET4 Dissipative Newmark" begin
     cp("../examples/ahead/overlap/bracket/dynamic/bracket.yaml", "bracket.yaml"; force=true)
     cp("../examples/ahead/overlap/bracket/dynamic/bracket-1.yaml", "bracket-1.yaml"; force=true)
     cp("../examples/ahead/overlap/bracket/dynamic/bracket-2.yaml", "bracket-2.yaml"; force=true)
@@ -15,8 +15,8 @@ using YAML
     input_file = "bracket.yaml"
     params = YAML.load_file(input_file; dicttype=Norma.Parameters)
     params["initial time"] = 0.0
-    params["time step"] = 1.0e-6
-    params["final time"] = 5.0e-5
+    params["time step"] = 1.0e-5
+    params["final time"] = 5.0e-4
     params["name"] = input_file
     sim = Norma.run(params)
     subsims = sim.subsims
@@ -49,29 +49,46 @@ using YAML
     println("avg_stress_bracket1 = ", avg_stress_bracket1, "\n")
     println("avg_stress_bracket2 = ", avg_stress_bracket2, "\n")
 
-    @test min_disp_x_bracket1 ≈ -3.239534728147805e-5 atol = 1e-12
-    @test min_disp_y_bracket1 ≈ -5.390040635191995e-5 atol = 1e-12
-    @test max_disp_z_bracket1 ≈ 0.0001136313123976402 atol = 1e-12
-    @test min_disp_x_bracket2 ≈ -0.00011223576082500242 atol = 1e-12
-    @test min_disp_y_bracket2 ≈ -5.617541938109094e-5 atol = 1e-12
-    @test max_disp_z_bracket2 ≈ 0.0008779212667680046 atol = 1e-12
-    @test avg_stress_bracket1 ≈ [-72712.82232539689 -90192.50915687135 4480.231591848708 27863.45785162852 -1.0987341235848952e6 -35974.56740305576] atol = 1e-12
-    @test avg_stress_bracket2 ≈ [1.4716110552352623e6 -3498.5059655966143 -380718.1494447633 -166878.95669528542 -4.988667087022611e6 -145718.2020229697] atol = 1e-12
-    @test sim.controller.schwarz_iters ≈ [2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3] atol = 0
+min_disp_x_bracket1 = -5.231281746226335e-5
+
+min_disp_y_bracket1 = -5.933051668002842e-6
+
+max_disp_z_bracket1 = 0.00014340372812408995
+
+min_disp_x_bracket2 = -0.0005079606839053646
+
+min_disp_y_bracket2 = -5.186627095656038e-5
+
+max_disp_z_bracket2 = 0.00759289102228929
+
+avg_stress_bracket1 = [-439706.2504282237 19682.643361487368 25543.12473412688 39089.560457661784 1.0441100533244014e7 1.5172514342809569e6]
+
+avg_stress_bracket2 = [-4.213060506205827e6 86462.63196185895 2.679828228066118e6 -774165.6773292988 1.7355828076996434e7 416148.063060136]
+
+
+    @test min_disp_x_bracket1 ≈ -5.231281746226335e-5 atol = 1e-12
+    @test min_disp_y_bracket1 ≈ -5.933051668002842e-6 atol = 1e-12
+    @test max_disp_z_bracket1 ≈ 0.00014340372812408995 atol = 1e-12
+    @test min_disp_x_bracket2 ≈ -0.0005079606839053646 atol = 1e-12
+    @test min_disp_y_bracket2 ≈ -5.186627095656038e-5 atol = 1e-12
+    @test max_disp_z_bracket2 ≈ 0.00759289102228929 atol = 1e-12
+    @test avg_stress_bracket1 ≈ [-439706.2504282237 19682.643361487368 25543.12473412688 39089.560457661784 1.0441100533244014e7 1.5172514342809569e6] atol = 1e-10
+    @test avg_stress_bracket2 ≈ [-4.213060506205827e6 86462.63196185895 2.679828228066118e6 -774165.6773292988 1.7355828076996434e7 416148.063060136] atol = 1e-10
+    @test sim.controller.schwarz_iters ≈ [3, 6, 8, 10, 11, 11, 10, 12, 12, 14, 15, 15, 11, 14, 15, 16, 15, 14, 13, 10, 10, 11, 11, 10, 9, 8, 6, 8, 9, 10, 10, 11, 11, 10, 10, 9, 7, 9, 11, 11, 12, 12, 11, 9, 9, 11, 12, 12, 12, 11] atol = 0
 
 end
 
-@testset "Schwarz AHeaD Overlap Dynamic Bracket HEX8-TET4 Dissipative Newmark" begin
+@testset "Schwarz AHeaD Overlap Dynamic Bracket TET4-TET4 Dissipative Newmark" begin
     cp("../examples/ahead/overlap/bracket/dynamic/bracket.yaml", "bracket.yaml"; force=true)
     cp("../examples/ahead/overlap/bracket/dynamic/bracket-1.yaml", "bracket-1.yaml"; force=true)
     cp("../examples/ahead/overlap/bracket/dynamic/bracket-2.yaml", "bracket-2.yaml"; force=true)
-    cp("../examples/ahead/overlap/bracket/bracket-1-hex.g", "../bracket-1.g"; force=true)
+    cp("../examples/ahead/overlap/bracket/bracket-1-tet.g", "../bracket-1.g"; force=true)
     cp("../examples/ahead/overlap/bracket/bracket-2-tet.g", "../bracket-2.g"; force=true)
     input_file = "bracket.yaml"
     params = YAML.load_file(input_file; dicttype=Norma.Parameters)
     params["initial time"] = 0.0
-    params["time step"] = 1.0e-6
-    params["final time"] = 5.0e-5
+    params["time step"] = 1.0e-5
+    params["final time"] = 5.0e-4
     params["name"] = input_file
     sim = Norma.run(params)
     subsims = sim.subsims
@@ -104,15 +121,32 @@ end
     println("avg_stress_bracket1 = ", avg_stress_bracket1, "\n")
     println("avg_stress_bracket2 = ", avg_stress_bracket2, "\n")
 
-    @test min_disp_x_bracket1 ≈ -3.255687122828699e-5 atol = 1e-12
-    @test min_disp_y_bracket1 ≈ -1.7910917248203928e-5 atol = 1e-12
-    @test max_disp_z_bracket1 ≈ 0.00012620821252524398 atol = 1e-12
-    @test min_disp_x_bracket2 ≈ -0.00017930987076132665 atol = 1e-12
-    @test min_disp_y_bracket2 ≈ -4.052985881622395e-5 atol = 1e-12
-    @test max_disp_z_bracket2 ≈ 0.0016696791571882948 atol = 1e-12
-    @test avg_stress_bracket1 ≈ [5261.147871837818 1.8229072090992345e6 165837.7136189107 256680.3604909731 245289.15366988638 -147308.3450816948] atol = 1e-12
-    @test avg_stress_bracket2 ≈ [4.982364543425214e6 2.8123926560396124e6 -405418.24914701295 1.3230848216495877e6 607114.2790286107 2.233382099904088e6] atol = 1e-12
-    @test sim.controller.schwarz_iters ≈ [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6] atol = 0
+min_disp_x_bracket1 = -0.00013489122345448565
+
+min_disp_y_bracket1 = -6.88670267136593e-5
+
+max_disp_z_bracket1 = 0.0019608960014893886
+
+min_disp_x_bracket2 = -0.00017674720316077086
+
+min_disp_y_bracket2 = -7.039442537440377e-5
+
+max_disp_z_bracket2 = 0.0037945898323606952
+
+avg_stress_bracket1 = [928393.0804969968 484726.67620943306 -86368.14791471219 39681.35369128554 2.8561263044046783e6 -84999.59564231828]
+
+avg_stress_bracket2 = [-498781.1969274577 380458.68521612795 342043.85822372086 18008.589711749457 1.654179334345221e6 -231776.00506447483]
+
+
+    @test min_disp_x_bracket1 ≈ -0.00013489122345448565 atol = 1e-12
+    @test min_disp_y_bracket1 ≈ -6.88670267136593e-5 atol = 1e-12
+    @test max_disp_z_bracket1 ≈ 0.0019608960014893886 atol = 1e-12
+    @test min_disp_x_bracket2 ≈ -0.00017674720316077086 atol = 1e-12
+    @test min_disp_y_bracket2 ≈ -7.039442537440377e-5 atol = 1e-12
+    @test max_disp_z_bracket2 ≈ 0.0037945898323606952 atol = 1e-12
+    @test avg_stress_bracket1 ≈ [928393.0804969968 484726.67620943306 -86368.14791471219 39681.35369128554 2.8561263044046783e6 -84999.59564231828] atol = 1e-10
+    @test avg_stress_bracket2 ≈ [-498781.1969274577 380458.68521612795 342043.85822372086 18008.589711749457 1.654179334345221e6 -231776.00506447483] atol = 1e-10
+    @test sim.controller.schwarz_iters ≈ [3, 3, 4, 4, 4, 3, 3, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 3, 3] atol = 0
 
 end
 
