7f78872a3b19d3f42ef13c30202bd1d2e1508b1e
Alejandro Mota <amota@sandia.gov>
2025-07-16 17:14:57 -0700

    Overhaul steepest descent solver for EMS (WIP)

--- src/model_types.jl
+++ src/model_types.jl
@@ -34,11 +34,9 @@ struct EvaluationFlags
     is_implicit::Bool
     is_hessian_opt::Bool
     is_matrix_free::Bool
-    need_diag_stiffness::Bool
     need_lumped_mass::Bool
     need_stiffness::Bool
     need_mass::Bool
-    compute_diag_stiffness::Bool
     compute_lumped_mass::Bool
     compute_stiffness::Bool
     compute_mass::Bool
@@ -48,17 +46,15 @@ end
 struct SMThreadLocalArrays{V,M}
     energy::Vector{Float64}
     internal_force::Vector{V}
-    diag_stiffness::Vector{V}
     lumped_mass::Vector{V}
     stiffness::Vector{M}
     mass::Vector{M}
 end
 
-struct SMElementThreadLocalArrays{T,DOFV,IFV,DSV,LMV,SM,MM}
+struct SMElementThreadLocalArrays{T,DOFV,IFV,LMV,SM,MM}
     energy::Vector{T}
     dofs::Vector{DOFV}
     internal_force::Vector{IFV}
-    diag_stiffness::Vector{DSV}
     lumped_mass::Vector{LMV}
     stiffness::Vector{SM}
     mass::Vector{MM}
@@ -78,14 +74,12 @@ mutable struct SolidMechanics <: Model
     stored_energy::Vector{Vector{Float64}}
     strain_energy::Float64
     stiffness::SparseMatrixCSC{Float64,Int64}
-    diag_stiffness::Vector{Float64}
     mass::SparseMatrixCSC{Float64,Int64}
     lumped_mass::Vector{Float64}
     body_force::Vector{Float64}
     free_dofs::BitVector
     time::Float64
     compute_stiffness::Bool
-    compute_diag_stiffness::Bool
     compute_mass::Bool
     compute_lumped_mass::Bool
     failed::Bool
