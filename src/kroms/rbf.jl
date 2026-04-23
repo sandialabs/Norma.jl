0db83c2978ee31707cb3b41c3661c99e51b34976
Alejandro N. Diaz <andiaz@sandia.gov>
2026-04-23 10:19:10 -0400

    removed rbfs/helpers.jl to consolidate files. verified that kernel roms still work with norma refactor.

--- src/kroms/rbf.jl
+++ src/kroms/rbf.jl
@@ -2,7 +2,6 @@
 # using Printf
 
 include("rbf_types.jl")
-include("helpers.jl")
 
 function evaluate_rbf_interpolant(interpolant::RBFKernelInterpolant, y::Vector{Float64})
     val, jac = evaluate_kernel_with_jacobian(interpolant.rbf, interpolant.training_data, y)
@@ -113,8 +112,48 @@ function compute_rbf_jacobian(
     return (-shape^2*deriv_over_r) .* transpose(X .- y)
 end
 
+# Compute distance matrix/vector --------------------------------------------------
+function compute_distances(
+    X::Union{Matrix{Float64},Vector{Float64}},
+    Y::Union{Matrix{Float64},Vector{Float64}},
+)
+    return sqrt.(compute_squared_distances(X, Y))
+end
+
+function compute_squared_distances(x::Vector{Float64}, y::Vector{Float64})
+    return sum((x-y) .^ 2)
+end
+
+function compute_squared_distances(X::Matrix{Float64}, y::Vector{Float64})
+    return [compute_squared_distances(collect(x), y) for x in eachcol(X)]
+end
+
+function compute_squared_distances(x::Vector{Float64}, Y::Matrix{Float64})
+    return compute_squared_distances(Y, x)
+end
+
+function compute_squared_distances(X::Matrix{Float64}, Y::Matrix{Float64})
+    return stack([compute_squared_distances(X, collect(y)) for y in eachcol(Y)])
+end
+
+function frobenius_norm(X::Matrix{Float64})
+    return sqrt(sum(X .^ 2))
+end
 
 # Testing ------------------------------------------------------------
+
+function test_compute_squared_distances()
+    X = [1.0 2.0 3.0; 1.0 0.0 2.0]
+    Y = [1.0 0.0; 1.0 2.0]
+    true_dists = [0.0 2.0; 2.0 8.0; 5.0 9.0]
+
+    computed_dists = compute_squared_distances(X, Y)
+    error = frobenius_norm(true_dists-computed_dists)
+
+    println("squared distance error = ", error)
+    return error < 1e-15
+end
+
 function fd_jac(f, x::Vector{Float64}, h::Float64)
     fx = f(x)
     nf = size(fx)[1]
@@ -157,7 +196,7 @@ function test_rbf()
 end
 
 function _test_fd_jac_for_decreasing_h(f, x, true_jac)
-    println("   h           error")
+    println("   h         jac error")
     println("----------------------")
     for i = 1:8
         h = 10.0^(-1.0*i)
@@ -181,3 +220,4 @@ end
 
 # test_rbf()
 # test_fd_jac()
+# println(test_compute_squared_distances())
