fd712a4cf13174f8171fd49fe48036cc13e30247
Irina Tezaur <ikalash@sandia.gov>
2026-05-28 13:37:27 -0700

    Adding testing of velo and acce errors to OpInf l2 error example.
    Adding comparisons to baselines for errors rather than just checking
    positivity.

--- test/opinf-schwarz-overlap-l2-error-cuboid-hex8.jl
+++ test/opinf-schwarz-overlap-l2-error-cuboid-hex8.jl
@@ -4,7 +4,7 @@
 # is released under the BSD license detailed in the file license.txt in the
 # top-level Norma.jl directory.
 
-@testset "Opinf Schwarz Overlap L2 Error Dynamic Cuboid Hex8 Same Step" begin
+@testset "Opinf Schwarz Overlap L2 Displacement Error Dynamic Cuboid Hex8 Same Step" begin
     example_dir = "../examples/ahead/overlap/cuboid/dynamic-linear-opinf-rom-overlap-l2-error"
     input_files = (
         "cuboid-1.yaml",
@@ -50,14 +50,15 @@
         ])
         @test isfinite(bc_fine.overlap_l2_error)
         @test isfinite(bc_coarse.overlap_l2_error)
-        @test bc_fine.overlap_l2_error >= 0.0
-        @test bc_coarse.overlap_l2_error >= 0.0
+        @test bc_fine.overlap_l2_error ≈ 0.2549171831856978 rtol = 1.0e-06
+        @test bc_coarse.overlap_l2_error ≈ 2.8375269021790513e-13 rtol = 1.0e-12
         @test isfile("overlap-l2-disp-rel-errors-0001.csv")
 
         overlap_csv = read("overlap-l2-disp-rel-errors-0001.csv", String)
         @test occursin("domain,side_set,overlap_l2_error", overlap_csv)
         @test occursin("cuboid-1,ssz+", overlap_csv)
         @test occursin("cuboid-2,ssz-", overlap_csv)
+
     finally
         for file in input_files
             rm(file; force=true)
@@ -71,3 +72,157 @@
         end
     end
 end
+
+@testset "Opinf Schwarz Overlap L2 Velocity Error Dynamic Cuboid Hex8 Same Step" begin
+    example_dir = "../examples/ahead/overlap/cuboid/dynamic-linear-opinf-rom-overlap-l2-error"
+    input_files = (
+        "cuboid-1.yaml",
+        "cuboid-2.yaml",
+        "cuboids.yaml",
+        "opinf-operator.npz",
+        "cuboid-1.g",
+        "cuboid-2.g",
+    )
+
+    for file in input_files
+        cp(joinpath(example_dir, file), file; force=true)
+    end
+        
+    for fname in ("cuboid-1.yaml", "cuboid-2.yaml")
+        write(fname, replace(read(fname, String),
+        "compute overlap L2 relative error: disp" =>
+        "compute overlap L2 relative error: velo"))
+    end
+
+    try
+        sim = Norma.create_simulation("cuboids.yaml")
+
+        bc_fine = only([
+            bc for bc in sim.subsims[1].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        bc_coarse = only([
+            bc for bc in sim.subsims[2].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        @test bc_fine.compute_overlap_l2_error == "velo"
+        @test bc_coarse.compute_overlap_l2_error == "velo"
+        @test !isempty(bc_fine.overlap_node_indices)
+        @test !isempty(bc_coarse.overlap_node_indices)
+        @test length(bc_fine.overlap_node_indices) > length(unique(bc_fine.side_set_node_indices))
+        Exodus.close(sim.subsims[1].params["input_mesh"])
+        Exodus.close(sim.subsims[1].params["output_mesh"])
+        Exodus.close(sim.subsims[2].params["input_mesh"])
+        Exodus.close(sim.subsims[2].params["output_mesh"])
+
+        sim = Norma.run("cuboids.yaml")
+        bc_fine = only([
+            bc for bc in sim.subsims[1].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        bc_coarse = only([
+            bc for bc in sim.subsims[2].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        @test isfinite(bc_fine.overlap_l2_error)
+        @test isfinite(bc_coarse.overlap_l2_error)
+        @test bc_fine.overlap_l2_error ≈ 0.2021333 rtol = 1.0e-06
+        @test bc_coarse.overlap_l2_error ≈ 0.0 atol = 0.0
+        @test isfile("overlap-l2-velo-rel-errors-0001.csv")
+
+        overlap_csv = read("overlap-l2-velo-rel-errors-0001.csv", String)
+        @test occursin("domain,side_set,overlap_l2_error", overlap_csv)
+        @test occursin("cuboid-1,ssz+", overlap_csv)
+        @test occursin("cuboid-2,ssz-", overlap_csv)
+
+    finally
+        for file in input_files
+            rm(file; force=true)
+        end
+        rm("cuboid-1.e"; force=true)
+        rm("cuboid-2.e"; force=true)
+        for file in readdir()
+            if startswith(file, "iterations-") || startswith(file, "overlap-l2-")
+                rm(file; force=true)
+            end
+        end
+    end
+end
+
+
+@testset "Opinf Schwarz Overlap L2 Acceleration Error Dynamic Cuboid Hex8 Same Step" begin
+    example_dir = "../examples/ahead/overlap/cuboid/dynamic-linear-opinf-rom-overlap-l2-error"
+    input_files = (
+        "cuboid-1.yaml",
+        "cuboid-2.yaml",
+        "cuboids.yaml",
+        "opinf-operator.npz",
+        "cuboid-1.g",
+        "cuboid-2.g",
+    )
+
+    for file in input_files
+        cp(joinpath(example_dir, file), file; force=true)
+    end
+        
+    for fname in ("cuboid-1.yaml", "cuboid-2.yaml")
+        write(fname, replace(read(fname, String),
+        "compute overlap L2 relative error: disp" =>
+        "compute overlap L2 relative error: acce"))
+    end
+
+    try
+        sim = Norma.create_simulation("cuboids.yaml")
+
+        bc_fine = only([
+            bc for bc in sim.subsims[1].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        bc_coarse = only([
+            bc for bc in sim.subsims[2].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        @test bc_fine.compute_overlap_l2_error == "acce"
+        @test bc_coarse.compute_overlap_l2_error == "acce"
+        @test !isempty(bc_fine.overlap_node_indices)
+        @test !isempty(bc_coarse.overlap_node_indices)
+        @test length(bc_fine.overlap_node_indices) > length(unique(bc_fine.side_set_node_indices))
+        Exodus.close(sim.subsims[1].params["input_mesh"])
+        Exodus.close(sim.subsims[1].params["output_mesh"])
+        Exodus.close(sim.subsims[2].params["input_mesh"])
+        Exodus.close(sim.subsims[2].params["output_mesh"])
+
+        sim = Norma.run("cuboids.yaml")
+        bc_fine = only([
+            bc for bc in sim.subsims[1].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        bc_coarse = only([
+            bc for bc in sim.subsims[2].model.boundary_conditions if
+            bc isa Norma.SolidMechanicsOverlapSchwarzBoundaryCondition
+        ])
+        @test isfinite(bc_fine.overlap_l2_error)
+        @test isfinite(bc_coarse.overlap_l2_error)
+        @test bc_fine.overlap_l2_error ≈ 0.1420472027022424 rtol = 1.0e-06
+        @test bc_coarse.overlap_l2_error ≈ 0.0 atol = 0.0
+        @test isfile("overlap-l2-acce-rel-errors-0001.csv")
+
+        overlap_csv = read("overlap-l2-acce-rel-errors-0001.csv", String)
+        @test occursin("domain,side_set,overlap_l2_error", overlap_csv)
+        @test occursin("cuboid-1,ssz+", overlap_csv)
+        @test occursin("cuboid-2,ssz-", overlap_csv)
+
+    finally
+        for file in input_files
+            rm(file; force=true)
+        end
+        rm("cuboid-1.e"; force=true)
+        rm("cuboid-2.e"; force=true)
+        for file in readdir()
+            if startswith(file, "iterations-") || startswith(file, "overlap-l2-")
+                rm(file; force=true)
+            end
+        end
+    end
+end
+
