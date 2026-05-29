40f1b0b0fd71ed6b52a5c737ea943030655993c4
eparish1 <eric.parish9@gmail.com>
2026-05-29 06:21:10 -0500

    updated nnopinf to work with SNL branch

--- examples/ahead/overlap/cuboid/dynamic-opinf-fom/make_nn_opinf_models.py
+++ examples/ahead/overlap/cuboid/dynamic-opinf-fom/make_nn_opinf_models.py
@@ -8,7 +8,7 @@ import numpy as np
 if __name__ == '__main__':
     settings = {}
     settings['fom-yaml-file'] = 'cuboid-2.yaml'
-    settings['training-data-directories'] = ['/Users/ejparis/Research/ahead/tmp/Norma.jl/examples/ahead/overlap/cuboid/dynamic-opinf-fom']
+    settings['training-data-directories'] = [os.getcwd()]
     settings['model-type'] = 'neural-network'
     settings['stop-training-time'] = 100000.0
     settings['training-skip-steps'] = 1
@@ -17,11 +17,12 @@ if __name__ == '__main__':
     settings['boundary-truncation-type'] = 'energy'
     settings['regularization-parameter'] = [0.0005, 0.005, 0.05]
     settings['model-name'] = 'opinf-operator'
-    settings['truncation-value'] = 1. - 1.e-5
-    settings['boundary-truncation-value'] = 1. - 1.e-5
-    settings['trial-space-splitting-type'] = 'split'
+    settings['truncation-value'] = 0.999999
+    settings['boundary-truncation-value'] = 0.999999
+    settings['trial-space-splitting-type'] = 'combined'
     settings['acceleration-computation-type'] = 'finite-difference'
-    settings['neural-network-training-settings'] = {'model-name': 'opinf-operator', 'output-path': 'ml-models', 'num-epochs': 15000, 'batch-size': 500, 'learning-rate': 0.001, 'weight-decay': 1e-08, 'lr-decay': 0.9999, 'print-training-output': True, 'epoch': 5000, 'resume': False}
-    settings['ensemble-size'] = 5
+    settings['neural-network-training-settings'] = {'model-name': 'opinf-operator', 'output-path': 'ml-models', 'print-training-output': True, 'resume': False, 'GN-final-layer': False, 'GN-num-layers': 0, 'GN-final-layer-epoch-frequency': 0, 'GN-final-layer-damping': 0.0, 'GN-verbose': False, 'optimizer': {'method': 'ADAM', 'num-epochs': 10000, 'batch-size': 500, 'learning-rate': 0.001, 'weight-decay': 1e-08, 'lr-decay': 0.9999, 'LBFGS-acceleration': {'enabled': True, 'acceleration-epoch-frequency': 1000, 'acceleration-iterations': 50}}}
+    settings['architecture'] = {'model-structure': 'PsdLagrangianOperator', 'n-hidden-layers': 2, 'n-neurons-per-layer': 'auto'}
+    settings['ensemble-size'] = 3
     snapshots_dict = normaopinf.opinf.get_processed_snapshots(settings)
     normaopinf.opinf.make_opinf_model_from_snapshots_dict(snapshots_dict, settings)
