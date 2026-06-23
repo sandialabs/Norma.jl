af41ce326ccc7ca46f39eddf3f808c35b310bccf
Irina Tezaur <ikalash@sandia.gov>
2026-06-22 21:54:23 -0700

    Creating ROM->FOM swapping test case for notched-cylinder.  Swapping is based on simple time criterion for now.  Linear elastic material model and linear OpInf.  Displacements are correct but accelerations are off in swapping domain after swap - same issue as clamped, likely.

--- examples/ahead/overlap/notched-cylinder/dynamic-opinf-fom/make_op_inf_models.py
+++ examples/ahead/overlap/notched-cylinder/dynamic-opinf-fom/make_op_inf_models.py
@@ -7,20 +7,20 @@ import numpy as np
 
 if __name__ == '__main__':
   settings = {}
-  settings['fom-yaml-file'] = "notched-cylinder-2.yaml"
+  settings['fom-yaml-file'] = "notched-cylinder-1.yaml"
   settings['training-data-directories'] = [os.getcwd()]
   settings['model-type'] = 'linear'
   settings['stop-training-time'] = 'end'
   settings['training-skip-steps'] = 1
   settings['forcing'] =  False
   settings['truncation-type'] = 'size'
-  settings['truncation-value'] = 30
-  settings['boundary-truncation-value'] = 4
-  settings['boundary-truncation-type'] =  'size'
-  settings['regularization-parameter'] =  1.e-8
-  settings['trial-space-splitting-type'] = 'split'
+  settings['truncation-value'] = 3
+  settings['boundary-truncation-value'] = 99.999
+  settings['boundary-truncation-type'] =  'energy'
+  settings['regularization-parameter'] =  [1.0e-1, 1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6, 1.0e-7, 1.0e-8, 1.0e-9, 1.0e-10]
+  settings['trial-space-splitting-type'] = 'combined'
   settings['acceleration-computation-type'] = 'acceleration-snapshots'
   snapshots_dict = normaopinf.opinf.get_processed_snapshots(settings)
-  settings['model-name'] = 'linear-opinf-operator'
+  settings['model-name'] = 'linear-opinf-operator-1'
   normaopinf.opinf.make_opinf_model_from_snapshots_dict(snapshots_dict,settings)
 
