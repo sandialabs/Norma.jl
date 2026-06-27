adc3aab0eb90dbdad0a55684c5323dfbd34b3692
Irina Tezaur <ikalash@sandia.gov>
2026-06-27 13:45:00 -0700

    Modifying single Gaussian problem to have left subdomain be smaller than the right one.

---
+++ examples/ahead/overlap/clamped/dynamic-linear-elastic-single-gaussian-opinf-fom/make_op_inf_models.py
@@ -0,0 +1,26 @@
+import normaopinf
+import normaopinf.opinf
+import nnopinf
+import nnopinf.training
+import os
+import numpy as np
+
+if __name__ == '__main__':
+  settings = {}
+  settings['fom-yaml-file'] = "clamped-1.yaml"
+  settings['training-data-directories'] = [os.getcwd()]
+  settings['model-type'] = 'linear'
+  settings['stop-training-time'] = 'end'
+  settings['training-skip-steps'] = 1
+  settings['forcing'] =  False
+  settings['truncation-type'] = 'size'
+  settings['boundary-truncation-type'] =  'size'
+  settings['truncation-value'] = 5
+  settings['boundary-truncation-value'] = 4
+  settings['regularization-parameter'] =  [1.0e-1, 1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6, 1.0e-7, 1.0e-8, 1.0e-9, 1.0e-10]
+  settings['trial-space-splitting-type'] = 'combined'
+  settings['acceleration-computation-type'] = 'acceleration-snapshots'
+  snapshots_dict = normaopinf.opinf.get_processed_snapshots(settings)
+  settings['model-name'] = 'linear-opinf-operator-1-M5'
+  normaopinf.opinf.make_opinf_model_from_snapshots_dict(snapshots_dict,settings)
+
